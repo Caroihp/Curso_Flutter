ef98fae517bcd5e8d57d4dabbca62a4bacad8dd0
Caro.ihp <carolina.hp251014@gmail.com>
2023-12-01 14:57:38 -0300

    Actualización widget3

--- widget3/lib/main.dart
+++ widget3/lib/main.dart
@@ -1,161 +1,113 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-void main() => runApp(CarouselDemo());
-
-final themeMode = ValueNotifier(2);
-
-class CarouselDemo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      builder: (context, value, g) {
-        return MaterialApp(
-          initialRoute: '/',
-          darkTheme: ThemeData.dark(),
-          themeMode: ThemeMode.values.toList()[value as int],
-          debugShowCheckedModeBanner: false,
-          routes: {
-            '/': (ctx) => CarouselDemoHome(),
-            '/complicated': (ctx) => ComplicatedImageDemo(),
-          },
-        );
-      },
-      valueListenable: themeMode,
-    );
-  }
+void main() {
+  runApp(const MyApp());
 }
 
-class DemoItem extends StatelessWidget {
-  final String title;
-  final String route;
-  DemoItem(this.title, this.route);
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      onTap: () {
-        Navigator.pushNamed(context, route);
-      },
-    );
-  }
-}
-
-class CarouselDemoHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pokedex'),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.nightlight_round),
-              onPressed: () {
-                themeMode.value = themeMode.value == 1 ? 2 : 1;
-              })
-        ],
+    return MaterialApp(
+      title: 'Pokédex',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: ListView(
-        children: <Widget>[
-          DemoItem('More complicated image slider', '/complicated'),
-        ],
-      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class ComplicatedImageDemo extends StatelessWidget {
-  ComplicatedImageDemo({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
 
-  // Suponiendo que esta es la estructura de tu lista de pokémon
-  final List<Map<String, dynamic>> pokemonList = [
-    {
-      'name': 'Bulbasaur',
-      'type': 'Planta',
-      'imageUrl': 'assets/Bulbasaur.png',
-      'number': '001' // Asegúrate de añadir el número de Pokedex
-    },
-    {
-      'name': 'Charmander',
-      'type': 'Fuego',
-      'imageUrl': 'assets/Charmander.png',
-      'number': '002' // Asegúrate de añadir el número de Pokedex
-    },
-    {
-      'name': 'Squirtle',
-      'type': 'Agua',
-      'imageUrl': 'assets/Squirtle.png',
-      'number': '003' // Asegúrate de añadir el número de Pokedex
-    }
+  final List<Pokemon> pokemons = [
+    Pokemon(name: 'Bulbasaur', type: 'Planta', number: 1, imagePath: 'assets/bulbasaur.png'),
+    Pokemon(name: 'Charmander', type: 'Fuego', number: 4, imagePath: 'assets/charmander.png'),
+    Pokemon(name: 'Squirtle', type: 'Agua', number: 7, imagePath: 'assets/squirtle.png'),
   ];
 
   @override
   Widget build(BuildContext context) {
-    // Este valor es clave para controlar el tamaño de la tarjeta en el carrusel
-    double viewportFractionValue = 0.75;
-
-    List<Widget> pokemonSliders = pokemonList.map((pokemon) {
-      // Configura el ancho de la tarjeta en función del ancho de la pantalla
-      double cardWidth = MediaQuery.of(context).size.width * viewportFractionValue;
-
-      return Container(
-        width: cardWidth, // Ajusta el ancho de la tarjeta aquí
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0), // Bordes redondeados
-          ),
-          elevation: 5, // Sombra de la tarjeta
-          child: Column(
-            mainAxisSize: MainAxisSize.min, // Asegura que la Columna no ocupe más espacio del necesario
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)), // Imagen con bordes redondeados en la parte superior
-                child: Image.asset(pokemon['imageUrl'], fit: BoxFit.cover),
-              ),
-              Padding(
-                padding: EdgeInsets.all(16.0), // Añade espacio alrededor del texto
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      pokemon['name'],
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 4), // Espacio entre el nombre y el tipo
-                    Text(
-                      'Tipo: ${pokemon['type']}',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 8), // Espacio entre el tipo y el número de Pokedex
-                    Text(
-                      'Pokedex: ${pokemon['number']}',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-    }).toList();
-
     return Scaffold(
-      appBar: AppBar(title: Text('Pokedex')),
-      body: CarouselSlider(
-        options: CarouselOptions(
-          autoPlay: false,
-          enlargeCenterPage: true,
-          aspectRatio: 2.0 / 1.0,
-          viewportFraction: viewportFractionValue, // Usa la variable aquí
+      appBar: AppBar(
+        title: const Text('Pokédex'),
+      ),
+      body: Center(
+        child: CarouselSlider.builder(
+          itemCount: pokemons.length,
+          itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+            return PokemonCard(pokemon: pokemons[itemIndex]);
+          },
+          options: CarouselOptions(
+            height: 400.0,
+            enlargeCenterPage: true,
+            autoPlay: false,
+            aspectRatio: 16/9,
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enableInfiniteScroll: true,
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            viewportFraction: 0.8,
+          ),
         ),
-        items: pokemonSliders,
       ),
     );
   }
 }
 
+class Pokemon {
+  final String name;
+  final String type;
+  final int number;
+  final String imagePath;
+
+  Pokemon({required this.name, required this.type, required this.number, required this.imagePath});
+}
+
+class PokemonCard extends StatelessWidget {
+  const PokemonCard({super.key, required this.pokemon});
+
+  final Pokemon pokemon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Expanded( // La imagen tomará todo el espacio vertical disponible restante
+            child: Image.asset(pokemon.imagePath, fit: BoxFit.contain),
+          ),
+          ListTile(
+            title: Text(pokemon.name, style: const TextStyle(fontWeight: FontWeight.bold)),
+            subtitle: Text('Pokédex: ${pokemon.number}'),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+              padding: const EdgeInsets.all(8),
+              color: _getTypeColor(pokemon.type),
+              child: Text('Tipo: ${pokemon.type}', style: const TextStyle(color: Colors.white)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Color _getTypeColor(String type) {
+    switch (type) {
+      case 'Planta':
+        return Colors.green;
+      case 'Fuego':
+        return Colors.orange;
+      case 'Agua':
+        return Colors.blue;
+      default:
+        return Colors.grey;
+    }
+  }
+}
